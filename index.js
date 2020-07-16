ee848677d3420000204ba499d1125acd797b3e00
vinhqd <vinhqd1996@gmail.com>
2020-07-16 22:24:12 +0700

    Create nodejsServer and connect socketio

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+io.on('connection', (socket) => {
+    console.log('Co nguoi ket noi id: ' + socket.id);
+    socket.on('disconnect', () => {
+        console.log(socket.id + ' da ngat ket noi')
+    })
+})
+
+server.listen(3000, () => console.log("Listening port 3000."));
+
+// router
+app.get('/', (req, res) => res.render('index'));
